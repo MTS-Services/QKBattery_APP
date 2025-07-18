ee1d3f6e67f162c7249cea90d84990d79110e25e
Jabed Jahangir <jabed6810@gmail.com>
2025-07-18 10:47:54 +0600

    fixed icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.batteryqk.batteryqk_app"
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
+        versionCode = 8
         versionName = "1.0.0"
         multiDexEnabled = true
     }
